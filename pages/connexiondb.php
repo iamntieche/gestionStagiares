bd962ba963a221fa1df48d2e30cb814308139afb
Ntieche_mfoum_issofa <youssouf.tiefa@gmail.com>
2019-12-03 07:12:06 +0100

    ajout du footer

--- pages/connexiondb.php
+++ pages/connexiondb.php
@@ -1,6 +1,6 @@
 <?php 
   try{
-  	$pdo= new PDO("mysql:host=localhost;dbname=gestion_stag","root",""); //object de con a la bd pr tt type de bd
+  	$pdo= new PDO("mysql:host=localhost;dbname=gestion_stag","youssouftiefa","1youss"); //object de con a la bd pr tt type de bd
   }catch(Exception $e){
       die('Erreur de connexion : '.$e->getMessage());
   }
